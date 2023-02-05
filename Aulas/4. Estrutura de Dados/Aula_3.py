ca5709e8066bd10725bef3aa1b3421051875830b
ValterNiloJr <valter.nilo.jr@gmail.com>
2023-02-05 19:54:27 -0300

    aula_4 - Estrutura de Dados

---
+++ Aulas/4. Estrutura de Dados/Aula_3.py
@@ -0,0 +1,34 @@
+#-----------------------------------------------------------------------------------------------------------------------------------#
+# 1. Sobre filas
+
+#    Escreva um programa que simule a distribuição de um grupo de pessoas em 2 filas de atendimento, prioritária ou regular, e consequentemente o atendimento delas. O programa deve obedecer às seguintes regras:
+     
+#    O grupo deve conter 20 pessoas
+#    Cada pessoa pode ser alocada em uma fila prioritária ou regular, com probabilidades de 30% e 70%, reespectivamente
+#    1 pessoa da fila regular deve ser atendida a cada 3 pessoas da fila prioritária
+#    Não havendo prioridades, as pessoas da fila regular podem ser atendidas
+#    import random
+#    random.choices(population=opções, weights=probabilidades, k=quantidades)
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+# 2. Sobre pilhas
+
+#    Escreva um programa que gere 50 números inteiros randômicos entre 0 e 100. Para cada número gerado, verifique as seguintes regras:
+     
+#    se o número for par, empilhe na pilha chamada par;
+#    se o número for ímpar, empilhe na pilha chamada ímpar;
+#    se o número for 0, 10, 50 ou 100, desempilhe um elemento de cada pilha. Caso a pilha esteja vazia, mostre uma mensagem de fila vazia na tela.
+#    Ao termino do programa, desempilhe todos os elementos das duas pilhas, imprimindo-os na tela.
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+# 3. Sobre listas encadeadas e pilhas
+
+#    Refatore a implementação de pilhas, utilizando listas encadeadas como estrutura de dados, ao invés de listas ou arrays.
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+# 4. Sobre deques
+
+#    Com base na teoria de deques e nas implementações de filas e pilhas, faça a implementação da estrutura de deques.
